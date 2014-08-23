03171920fcd1de81ebf1304b23e0ac3e50640f6e
Brady Wied <brady@wied.us>
2014-08-23 17:39:32 -0500

    Refactored such that our resource updated result works properly, all test kitchen tests now pass

--- libraries/provider_wrapped_file.rb
+++ libraries/provider_wrapped_file.rb
@@ -8,37 +8,10 @@ class Chef
       use_inline_resources
 
       def action_install
-        converge_by "Replacing #{@new_resource.target_file} with wrapper" do
-          # Can't access scope of this class methods inside the resource
-          parent_resource = @new_resource
-          wrapper_cmd = wrapper_command
-          target_file = @new_resource.target_file
-          temp = wrapper_cmd + '.tmp'
-          ruby_block "Copying #{target_file} to #{temp}" do
-            block do
-              ::FileUtils.cp target_file, temp
-            end
-          end
-
-          resource_backup_real_exe = "Moving #{target_file} to #{wrapper_cmd}"
-          ruby_block resource_backup_real_exe do
-            action :nothing
-            block do
-              ::FileUtils.mv target_file, wrapper_cmd
-            end
-          end
-
-          template_source = parent_resource.source || base_target_file
-          template temp do
-            cookbook parent_resource.cookbook if parent_resource.cookbook
-            source template_source
-            variables :wrapped_file => wrapper_cmd
-            notifies :run, "ruby_block[#{resource_backup_real_exe}]", :immediately
-          end
-
-          ruby_block "Moving #{temp} back to #{target_file}" do
-            block do
-              ::FileUtils.mv temp, target_file
+        with_temporary_resource do |template|
+          if template.updated_by_last_action?
+            converge_by "Replacing #{@new_resource.target_file} with wrapper" do
+              ::FileUtils.mv @new_resource.target_file, wrapped_file
             end
           end
         end
@@ -46,12 +19,39 @@ class Chef
 
       private
 
+      def with_temporary_resource
+        temporary_file_path = temporary_file_name
+        ::FileUtils.cp @new_resource.target_file, temporary_file_path
+        begin
+          temp = temp_resource temporary_file_path
+          temp.run_action :create
+          yield temp
+        ensure
+          ::FileUtils.mv temporary_file_path, @new_resource.target_file
+        end
+      end
+
+      def temporary_file_name
+        Dir::Tmpname.create([::File.basename(@new_resource.target_file),'.tmp']) {}
+      end
+
+      def temp_resource(temporary_file_path)
+        wrapped_file_copy = wrapped_file
+        parent_resource = @new_resource
+        template_source = parent_resource.source || base_target_file
+        template temporary_file_path do
+          cookbook parent_resource.cookbook if parent_resource.cookbook
+          source template_source
+          variables :wrapped_file => wrapped_file_copy
+        end
+      end
+
       def base_target_file
         filename = ::File.basename @new_resource.target_file
         "#{filename}.erb"
       end
 
-      def wrapper_command
+      def wrapped_file
         base_file = ::File.basename(@new_resource.target_file)
         wrapper_base = "#{base_file}-wrapped"
         ::File.join(::File.dirname(@new_resource.target_file), wrapper_base)
